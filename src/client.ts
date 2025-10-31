02323800fb3a77e8d64c297ee9c199aff342cc7e
Kevin Cui <bh@bugs.cc>
2025-10-31 16:19:58 +0800

    feat: support update credentials (#4)
    Signed-off-by: Kevin Cui <bh@bugs.cc>

--- src/client.ts
+++ src/client.ts
@@ -3,7 +3,17 @@ import { Request } from "./request";
 
 export class Client {
     public request: Request;
-    public constructor(public readonly config: ClientConfig) {
+    public constructor(private config: ClientConfig) {
         this.request = new Request(config);
+    }
+
+    public updateCredentials(accessKeyID: string, accessKeySecret: string, stsToken?: string) {
+        this.config = {
+            ...this.config,
+            accessKeyID,
+            accessKeySecret,
+            stsToken,
+        };
+        this.request = new Request(this.config);
     }
 }
